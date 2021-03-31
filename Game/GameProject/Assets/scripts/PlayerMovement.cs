2a8560b9c1e316a9dfaaa10d8f22bb786743f9e5
Lucas Garlaschi <lucas.garlaschi@yahoo.com>
2021-03-31 22:57:56 +0200

    mouvement perso (encure un peu buggé)

---
+++ Game/GameProject/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        MovePlayer(horizontalMovement);
+
+        Flip(rb.velocity.x);
+
+        float charVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", charVelocity);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
