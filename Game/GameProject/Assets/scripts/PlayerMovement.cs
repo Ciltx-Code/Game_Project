194e04453dd4a5d8423727b2264781a992f76324
Lucas Garlaschi <lucas.garlaschi@yahoo.com>
2021-04-01 21:47:37 +0200

    réglages déplacement

--- Game/GameProject/Assets/scripts/PlayerMovement.cs
+++ Game/GameProject/Assets/scripts/PlayerMovement.cs
@@ -4,34 +4,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float airSpeed;
     public float jumpForce;
 
     public bool isJumping;
     public bool isGrounded;
 
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayer;
 
+    public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
-    public Rigidbody2D rb;
+   
+
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
 
-
-    void FixedUpdate()
+    void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
-
-
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
-
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
 
-        MovePlayer(horizontalMovement);
+        
 
         Flip(rb.velocity.x);
 
@@ -39,17 +37,27 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", charVelocity);
     }
 
+    void FixedUpdate()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayer);
+
+        
+        MovePlayer(horizontalMovement);
+    }
+
+    
 
     void MovePlayer(float _horizontalMovement)
     {
         
-
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-        if (isJumping)
+        if (isJumping == true)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
+            rb.AddForce(new Vector2(-0.5f, jumpForce));
             isJumping = false;
         }
     }
@@ -63,5 +71,11 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
